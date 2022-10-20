f90bad46391a9dfee3d475cd5e8575a52d7f7c96
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-10-20 13:54:25 +0600

    удаление всех изменений

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,23 +1,3 @@
 package main
 
-import (
-	"fmt"
-
-	gofermart "github.com/AltynayK/go-musthave-diploma-tpl"
-	"github.com/AltynayK/go-musthave-diploma-tpl/configs"
-	"github.com/AltynayK/go-musthave-diploma-tpl/pkg/handler"
-	"github.com/AltynayK/go-musthave-diploma-tpl/pkg/repository"
-	"github.com/AltynayK/go-musthave-diploma-tpl/pkg/service"
-)
-
-func main() {
-	config := configs.NewConfig()
-	db := repository.NewPostgresDB(config)
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
-	srv := new(gofermart.Server)
-	if err := srv.Run(config.RunAddress, handlers.InitRoutes()); err != nil {
-		fmt.Print(err)
-	}
-}
+func main() {}
--- user.go
+++
@@ -1,8 +0,0 @@
-package gofermart
-
-type User struct {
-	Id       int    `json:"-" db:"id"`
-	Login    string `json:"login" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Current  int    `json:"current"`
-}
